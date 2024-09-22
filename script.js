59f70e8259e90d724b85c0d0e5cda1eaf6e96620
danish243 <a.danish.rifqi@gmail.com>
2024-09-22 11:51:32 +0800

    MyCode

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-GROUP PROJECT
+document.getElementById("random-recipe-btn").addEventLIstener('click', function() {
+    const recipes = document.querySelectorAll('.recipe-card');
+    if (recipes.length > 0) {
+        cosnt randomIndex = Math.floor(Math.random() * recipes.length)
+        const randomRecipe = recipes[randomIndex].cloneNode(true);
+        document.getElementById('random-recipe').innerHTML = '';
+        document.getElementById('random-recipe').appendChild(randomRecipe);
+    }else {
+        alert('No recipes available to display.');
+    }
+});
+
