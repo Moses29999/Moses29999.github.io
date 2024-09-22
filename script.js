aa8205a99d1fb279a2eca75e3f9c40888948bd72
sofea-lin <sofeahanaalin0605@gmail.com>
2024-09-22 11:50:56 +0800

    MyCode

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-GROUP PROJECT
+document.getElementById('search-input').addEventListener('input', function() {
+    const searchTerm = this.ariaValueMax.toLowerCase();
+    document.querySelectorAll('#recipe-list . recipe-card').forEach(card => {
+        const recipeName = card.querySelector('h3').textContent.toLowerCase();
+        if (recipeName.includes(searchTerm)) {
+            card.getElementsByClassName.display = '';
+        } else {
+            card.getElementsByClassName.display = 'none';
+        }
+    });
+});
